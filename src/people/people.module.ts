40f07cc15be6ee9b9e30eeffeddd0b1a5ba4c691
claudiomelendres <claudiomelendres@gmail.com>
2023-12-29 08:57:00 -0400

    fixed prettier errors

--- src/people/people.module.ts
+++ src/people/people.module.ts
@@ -1,17 +1,16 @@
-import { Module } from '@nestjs/common';
-import { PeopleService } from './people.service';
-import { PeopleController } from './people.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Person, PersonSchema } from './entities/person.entity';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from "@nestjs/common"
+import { PeopleService } from "./people.service"
+import { PeopleController } from "./people.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Person, PersonSchema } from "./entities/person.entity";
+import { ConfigModule } from "@nestjs/config";
 
+// eslint-disable-next-line prettier/prettier
 @Module({
   controllers: [PeopleController],
   providers: [PeopleService],
-  imports: [
-    ConfigModule,
-    MongooseModule.forFeature([{ name: Person.name, schema: PersonSchema }]),
-  ],
+  imports: [ConfigModule, MongooseModule.forFeature([{ name: Person.name, schema: PersonSchema }])],
   exports: [MongooseModule],
 })
 export class PeopleModule { }
+
--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -24,79 +24,79 @@ export class PeopleService {
   ) {
     // console.log(process.env.DEFAULT_LIMIT)
     this.defaultLimit = this.configService.get<number>('defaultLimit');
-  //   console.log(this.defaultLimit);
-  // }
+    //   console.log(this.defaultLimit);
+  }
 
   async create(createPersonDto: CreatePersonDto) {
-      createPersonDto.name = createPersonDto.name.toLocaleLowerCase();
+    createPersonDto.name = createPersonDto.name.toLocaleLowerCase();
 
-      try {
-        const person = await this.personModel.create(createPersonDto);
-        return person;
-      } catch (error) {
-        this.handleExceptions(error);
-      }
+    try {
+      const person = await this.personModel.create(createPersonDto);
+      return person;
+    } catch (error) {
+      this.handleExceptions(error);
     }
+  }
 
-    findAll(paginationQuery: PaginationDto) {
-      const { limit = this.defaultLimit, offset = 0 } = paginationQuery;
-      return this.personModel
-        .find()
-        .limit(limit)
-        .skip(offset)
-        .sort({ no: 1 })
-        .select('-__v');
-    }
+  findAll(paginationQuery: PaginationDto) {
+    const { limit = this.defaultLimit, offset = 0 } = paginationQuery;
+    return this.personModel
+      .find()
+      .limit(limit)
+      .skip(offset)
+      .sort({ no: 1 })
+      .select('-__v');
+  }
 
   async findOne(id: string) {
-      let person: Person;
+    let person: Person;
 
-      if (!isNaN(+id)) {
-        person = await this.personModel.findOne({ no: id });
-      }
-
-      // MongoId
-      if (!person && isValidObjectId(id)) {
-        person = await this.personModel.findById(id);
-      }
-
-      // Name
-      if (!person) {
-        person = await this.personModel.findOne({
-          name: id.toLowerCase().trim(),
-        });
-      }
-
-      if (!person) throw new NotFoundException(`Person with id ${id} not found`);
-      return person;
+    if (!isNaN(+id)) {
+      person = await this.personModel.findOne({ no: id });
     }
+
+    // MongoId
+    if (!person && isValidObjectId(id)) {
+      person = await this.personModel.findById(id);
+    }
+
+    // Name
+    if (!person) {
+      person = await this.personModel.findOne({
+        name: id.toLowerCase().trim(),
+      });
+    }
+
+    if (!person) throw new NotFoundException(`Person with id ${id} not found`);
+    return person;
+  }
 
   async update(id: string, updatePersonDto: UpdatePersonDto) {
-      const person = await this.findOne(id);
-      if (updatePersonDto.name)
-        updatePersonDto.name = updatePersonDto.name.toLocaleLowerCase();
+    const person = await this.findOne(id);
+    if (updatePersonDto.name)
+      updatePersonDto.name = updatePersonDto.name.toLocaleLowerCase();
 
-      try {
-        await person.updateOne(updatePersonDto);
-        return { ...person.toJSON(), ...updatePersonDto };
-      } catch (error) {
-        this.handleExceptions(error);
-      }
+    try {
+      await person.updateOne(updatePersonDto);
+      return { ...person.toJSON(), ...updatePersonDto };
+    } catch (error) {
+      this.handleExceptions(error);
     }
+  }
 
   async remove(id: string) {
-      // const person = await this.findOne(id);
-      // await person.deleteOne();
-      // return { id };
+    // const person = await this.findOne(id);
+    // await person.deleteOne();
+    // return { id };
 
-      // const result = await this.personModel.findByIdAndDelete(id);
-      // return result;
+    // const result = await this.personModel.findByIdAndDelete(id);
+    // return result;
 
-      const { deletedCount } = await this.personModel.deleteOne({ _id: id });
-      if (deletedCount === 0)
-        throw new NotFoundException(`Person with id ${id} not found`);
-      return { id };
-    }
+    const { deletedCount } = await this.personModel.deleteOne({ _id: id });
+    if (deletedCount === 0)
+      throw new NotFoundException(`Person with id ${id} not found`);
+    return { id };
+  }
 
   // para manejar el error de duplicidad de datos
   private handleExceptions(error: any) {
